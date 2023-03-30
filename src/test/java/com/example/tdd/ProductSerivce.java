3d076f52ccb37dd29e1331930aeffc13b503432f
KIM-DO-HEE <devkdhee@gmail.com>
2023-03-30 22:59:49 +0900

    add : 상품 등록 서비스 구현
    상품 등록 서비스 구현

--- src/test/java/com/example/tdd/ProductSerivce.java
+++ src/test/java/com/example/tdd/ProductSerivce.java
@@ -1,19 +1,20 @@
 package com.example.tdd;
 
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
 class ProductSerivce {
-    private final ProductServiceTest productServiceTest;
     private final ProductPort productPort;
 
-    public void addProduct(final ProductServiceTest.AddProductRequest request) {
-//            throw new UnsupportedOperationException("Unsupportd AddProductRequest");
-        final Product product = new Product(request.getName(), request.getPrice(), request.getDiscountPolicy());
-
-        productServiceTest.productRepository.save(product);
+    public ProductSerivce(final ProductPort productPort) {
+        this.productPort = productPort;
     }
 
-    public ProductSerivce(ProductServiceTest productServiceTest, final ProductPort productPort) {
-        this.productServiceTest = productServiceTest;
-        this.productPort = productPort;
+    @Transactional
+    public void addProduct(final AddProductRequest request) {
+        final Product product = new Product(request.getName(), request.getPrice(), request.getDiscountPolicy());
+        productPort.save(product);
     }
 
 }
